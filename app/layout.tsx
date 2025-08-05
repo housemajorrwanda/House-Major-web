e6e3f098774644bc90104a02da07bf66131b462a
mbarndouka <mmbarndouka@gmail.com>
2025-08-05 23:00:40 +0200

    comming soon page

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const kumbhSans = localFont({
+  src: [
+    {
+      path: "../assets/fonts/KumbhSans-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/KumbhSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/KumbhSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-kumbh-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Major House",
+  description: "Coming Soon - Major House",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${kumbhSans.variable} antialiased font-sans`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
